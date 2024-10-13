86b9922cf819f380ab7ae02612e4ad384a01a7b3
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-10-13 19:33:33 +0700

    Update tiktok.js

--- events/tiktok.js
+++ events/tiktok.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
 const gradient = require('gradient-string');
-
 module.exports = {
     name: 'tiktokauto',
     ver: '1.0',
@@ -25,56 +24,30 @@ async function downloadAndSendTikTokContent(url, api, threadID, messageID) {
         const response = await axios.post(`https://www.tikwm.com/api/`, { url: url });
         const data = response.data.data;
 
-        // Download video
         const videoStream = await axios({
             method: 'get',
             url: data.play,
             responseType: 'stream'
         }).then(res => res.data);
 
-        const videoFileName = `TikTok-${Date.now()}.mp4`;
-        const videoFilePath = `./${videoFileName}`;
-        const videoFile = fs.createWriteStream(videoFilePath);
+        const fileName = `TikTok-${Date.now()}.mp4`;
+        const filePath = `./${fileName}`;
+        const videoFile = fs.createWriteStream(filePath);
+
         videoStream.pipe(videoFile);
 
-        // Download images (assuming data.images is an array of image URLs)
-        const imageFilePaths = [];
-        const downloadImagePromises = data.images.map((imageUrl, index) => {
-            const imageFileName = `TikTok-${Date.now()}-${index}.jpg`;
-            const imageFilePath = `./${imageFileName}`;
-            imageFilePaths.push(imageFilePath);
+        videoFile.on('finish', () => {
+            videoFile.close(() => {
+                console.log('Downloaded TikTok video file.');
 
-            return axios({
-                method: 'get',
-                url: imageUrl,
-                responseType: 'stream'
-            }).then(res => {
-                const imageFile = fs.createWriteStream(imageFilePath);
-                res.data.pipe(imageFile);
-                return new Promise((resolve) => {
-                    imageFile.on('finish', resolve);
-                });
+                api.sendMessage({
+                    body: `𝗧𝗶𝗸𝘁𝗼𝗸 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿 𝗔𝘂𝘁𝗼\n━━━━━━━━━━━━━━━━━━\n\n𝙲𝚘𝚗𝚝𝚎𝚗𝚝: ${data.title}\n\n𝙻𝚒𝚔𝚎𝚜: ${data.digg_count}\n\n𝙲𝚘𝚖𝚖𝚎𝚗𝚝𝚜: ${data.comment_count}`,
+                    attachment: fs.createReadStream(filePath)
+                }, threadID, () => {
+                    fs.unlinkSync(filePath);
+                }, messageID);
             });
         });
-
-        // Wait for both video and images to be downloaded
-        await Promise.all([new Promise((resolve) => videoFile.on('finish', resolve)), ...downloadImagePromises]);
-
-        // Send the video and all images as attachments
-        const attachments = [
-            fs.createReadStream(videoFilePath),
-            ...imageFilePaths.map(filePath => fs.createReadStream(filePath))
-        ];
-
-        api.sendMessage({
-            body: `𝗧𝗶𝗸𝘁𝗼𝗸 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿 𝗔𝘂𝘁𝗼\n━━━━━━━━━━━━━━━━━━\n\n𝙲𝚘𝚗𝚝𝚎𝚗𝚝: ${data.title}\n\n𝙻𝚒𝚔𝚎𝚜: ${data.digg_count}\n\n𝙲𝚘𝚖𝚖𝚎𝚗𝚝𝚜: ${data.comment_count}`,
-            attachment: attachments
-        }, threadID, () => {
-            // Clean up downloaded files
-            fs.unlinkSync(videoFilePath);
-            imageFilePaths.forEach(filePath => fs.unlinkSync(filePath));
-        }, messageID);
-
     } catch (e) {
         console.log(e);
     }
