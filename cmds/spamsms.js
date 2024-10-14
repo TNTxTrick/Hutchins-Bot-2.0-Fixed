b07c94a2a9b451608437b151251ad56f109640f2
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-10-14 12:46:56 +0700

    Update spamsms.js

--- cmds/spamsms.js
+++ cmds/spamsms.js
@@ -8,7 +8,6 @@ module.exports = {
   cooldowns: 10,
 
   onLaunch: function ({ api, event }) {
-    // Extract the phone number from the command
     const args = event.body.trim().split(" ");
     if (args.length < 2) {
       return api.sendMessage("Vui lòng nhập số điện thoại. Ví dụ: spamsms 0123456789", event.threadID, event.messageID);
@@ -21,17 +20,11 @@ module.exports = {
       {
         headers: {
           "Host": "fptshop.com.vn",
-          "Content-Length": "16",
           "Accept": "*/*",
           "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
           "X-Requested-With": "XMLHttpRequest",
-          "Sec-CH-UA-Mobile": "?1",
-          "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; CPH1805) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36",
-          "Sec-CH-UA-Platform": "\"Linux\"",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
           "Origin": "https://fptshop.com.vn",
-          "Sec-Fetch-Site": "same-origin",
-          "Sec-Fetch-Mode": "cors",
-          "Sec-Fetch-Dest": "empty",
           "Referer": "https://fptshop.com.vn/",
           "Accept-Encoding": "gzip, deflate, br"
         }
